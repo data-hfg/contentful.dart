dc615c8ae529c007610357d37a6e12e107ed899e
minikin <djminikin@gmail.com>
2019-08-02 12:36:48 +0200

    Add flutter example

--- lib/contentful.dart
+++ lib/contentful.dart
@@ -1,4 +1,5 @@
 library contentful;
 
-export 'package:contentful.dart/src/models/models.dart';
 export 'package:contentful.dart/src/configuration/host.dart';
+export 'package:contentful.dart/src/models/models.dart';
+export 'package:contentful.dart/src/networking/contentful_clinet.dart';
