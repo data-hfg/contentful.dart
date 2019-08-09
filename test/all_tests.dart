8b9d51259ed5762ba41421e45af52d9938b33291
Sasha Prokhorenko <djminikin@gmail.com>
2019-08-09 16:25:23 +0200

    Update all_tests, travis.yml

---
+++ test/all_tests.dart
@@ -0,0 +1,13 @@
+import './models/all_locales_test.dart' as all_locales;
+import './models/asset_test.dart' as asset;
+import './models/locale_test.dart' as locale;
+import './models/space_test.dart' as space;
+import './models/system_fields_test.dart' as sys;
+
+void main() {
+  all_locales.main();
+  asset.main();
+  locale.main();
+  space.main();
+  sys.main();
+}
