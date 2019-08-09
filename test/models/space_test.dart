d329042ecc6b3464ae254adaf14f8f6450eefaea
Sasha Prokhorenko <djminikin@gmail.com>
2019-08-09 15:54:22 +0200

    Add space_test

---
+++ test/models/space_test.dart
@@ -0,0 +1,15 @@
+import 'package:contentful_dart/contentful.dart';
+import 'package:test/test.dart';
+
+import '../utils/load_fixture.dart';
+
+void main() {
+  group('Space Tests', () {
+    test('returns Space from json string', () {
+      final space = Space.fromJson(loadFixture('space'));
+      final spaceString = space.toJson();
+
+      expect(spaceString, const TypeMatcher<String>());
+    });
+  });
+}
