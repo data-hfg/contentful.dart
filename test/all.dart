b1649f98243e67d4b581b5b53d49237117f28afa
Sasha Prokhorenko <djminikin@gmail.com>
2019-08-14 09:40:36 +0200

    Add entry_test

--- test/all.dart
+++ test/all.dart
@@ -1,5 +1,6 @@
 import './models/all_locales_test.dart' as all_locales;
 import './models/asset_test.dart' as asset;
+import './models/entry_test.dart' as entry;
 import './models/locale_test.dart' as locale;
 import './models/location_test.dart' as location;
 import './models/space_test.dart' as space;
@@ -8,8 +9,9 @@ import './models/system_fields_test.dart' as sys;
 void main() {
   all_locales.main();
   asset.main();
+  entry.main();
   locale.main();
+  location.main();
   space.main();
   sys.main();
-  location.main();
 }
