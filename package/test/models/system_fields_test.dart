2dd1a2ae81ca4f091c8591e1587896c47b739d44
minikin <djminikin@gmail.com>
2019-08-19 12:25:48 +0200

    Update SystemFields tests

--- package/test/models/system_fields_test.dart
+++ package/test/models/system_fields_test.dart
@@ -4,9 +4,16 @@ import 'package:test/test.dart';
 import '../utils/load_fixture.dart';
 
 void main() {
-  group('SystemFields', () {
-    test('returns SystemFields from json string', () {
+  group('SystemFields Tests', () {
+    test('returns instance of SystemFields from json', () {
       final sys = SystemFields.fromJson(loadFixture('sys'));
+
+      expect(sys, const TypeMatcher<SystemFields>());
+    });
+
+    test('returns json string from SystemFields', () {
+      final sys = SystemFields.fromJson(loadFixture('location'));
+
       final sysString = sys.toJson();
 
       expect(sysString, const TypeMatcher<String>());
