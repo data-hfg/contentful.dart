e20919427ff17ee239b5ef8f5a0fd945c6a590ee
minikin <djminikin@gmail.com>
2019-08-28 13:28:06 +0200

    Update test for ContentType, ContentTypeResponse

--- package/test/models/content_type_response_test.dart
+++ package/test/models/content_type_response_test.dart
@@ -1,3 +1,5 @@
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value_test/matcher.dart';
 import 'package:contentful_dart/contentful_dart.dart';
 import 'package:test/test.dart';
 
@@ -19,6 +21,255 @@ void main() {
       final contentTypeResponseString = contentTypeResponse.toJson();
 
       expect(contentTypeResponseString, const TypeMatcher<String>());
+    });
+
+    test('built_value matcher', () {
+      final field = Field(
+        (b) => b
+          ..disabled = true
+          ..id = 'id'
+          ..isRequired = true
+          ..itemType = FieldType.boolean
+          ..localized = true
+          ..name = 'name'
+          ..type = FieldType.asset,
+      );
+
+      final contentType = ContentType(
+        (b) => b
+          ..description = 'description'
+          ..name = 'name'
+          ..sys = SystemFields(
+            (b) => b
+              ..contentType = 'contentType'
+              ..contentTypeId = 'contentTypeId'
+              ..createdAt = 'createdAt'
+              ..createdBy = 'createdBy'
+              ..firstPublishedAt = 'firstPublishedAt'
+              ..id = 'id'
+              ..locale = 'locale'
+              ..publishedAt = 'publishedAt'
+              ..publishedBy = 'publishedBy'
+              ..publishedCounter = 1
+              ..publishedVersion = 2
+              ..revision = 3
+              ..type = 'type'
+              ..updatedAt = 'updatedAt'
+              ..updatedBy = 'updatedBy'
+              ..version = 1,
+          ).toBuilder()
+          ..fields = BuiltList<Field>.from(
+            [field],
+          ).toBuilder(),
+      );
+
+      final value = ContentTypeResponse(
+        (b) => b
+          ..limit = 0
+          ..skip = 0
+          ..total = 100
+          ..sys = SystemFields(
+            (b) => b
+              ..contentType = 'contentType'
+              ..contentTypeId = 'contentTypeId'
+              ..createdAt = 'createdAt'
+              ..createdBy = 'createdBy'
+              ..firstPublishedAt = 'firstPublishedAt'
+              ..id = 'id'
+              ..locale = 'locale'
+              ..publishedAt = 'publishedAt'
+              ..publishedBy = 'publishedBy'
+              ..publishedCounter = 1
+              ..publishedVersion = 2
+              ..revision = 3
+              ..type = 'type'
+              ..updatedAt = 'updatedAt'
+              ..updatedBy = 'updatedBy'
+              ..version = 1,
+          ).toBuilder()
+          ..items = BuiltList<ContentType>.from(
+            [contentType],
+          ).toBuilder(),
+      );
+
+      expect(value, equalsBuilt(value));
+    });
+
+    test('compared value matcher', () {
+      final field = Field(
+        (b) => b
+          ..disabled = true
+          ..id = 'id'
+          ..isRequired = true
+          ..itemType = FieldType.boolean
+          ..localized = true
+          ..name = 'name'
+          ..type = FieldType.asset,
+      );
+
+      final contentType = ContentType(
+        (b) => b
+          ..description = 'description'
+          ..name = 'name'
+          ..sys = SystemFields(
+            (b) => b
+              ..contentType = 'contentType'
+              ..contentTypeId = 'contentTypeId'
+              ..createdAt = 'createdAt'
+              ..createdBy = 'createdBy'
+              ..firstPublishedAt = 'firstPublishedAt'
+              ..id = 'id'
+              ..locale = 'locale'
+              ..publishedAt = 'publishedAt'
+              ..publishedBy = 'publishedBy'
+              ..publishedCounter = 1
+              ..publishedVersion = 2
+              ..revision = 3
+              ..type = 'type'
+              ..updatedAt = 'updatedAt'
+              ..updatedBy = 'updatedBy'
+              ..version = 1,
+          ).toBuilder()
+          ..fields = BuiltList<Field>.from(
+            [field],
+          ).toBuilder(),
+      );
+
+      final value = ContentTypeResponse(
+        (b) => b
+          ..limit = 0
+          ..skip = 0
+          ..total = 100
+          ..sys = SystemFields(
+            (b) => b
+              ..contentType = 'contentType'
+              ..contentTypeId = 'contentTypeId'
+              ..createdAt = 'createdAt'
+              ..createdBy = 'createdBy'
+              ..firstPublishedAt = 'firstPublishedAt'
+              ..id = 'id'
+              ..locale = 'locale'
+              ..publishedAt = 'publishedAt'
+              ..publishedBy = 'publishedBy'
+              ..publishedCounter = 1
+              ..publishedVersion = 2
+              ..revision = 3
+              ..type = 'type'
+              ..updatedAt = 'updatedAt'
+              ..updatedBy = 'updatedBy'
+              ..version = 1,
+          ).toBuilder()
+          ..items = BuiltList<ContentType>.from(
+            [contentType],
+          ).toBuilder(),
+      );
+
+      final otherValue = ContentTypeResponse(
+        (b) => b
+          ..limit = 0
+          ..skip = 0
+          ..total = 100
+          ..sys = SystemFields(
+            (b) => b
+              ..contentType = 'contentType'
+              ..contentTypeId = 'contentTypeId'
+              ..createdAt = 'createdAt'
+              ..createdBy = 'createdBy'
+              ..firstPublishedAt = 'firstPublishedAt'
+              ..id = 'id'
+              ..locale = 'locale'
+              ..publishedAt = 'publishedAt'
+              ..publishedBy = 'publishedBy'
+              ..publishedCounter = 1
+              ..publishedVersion = 2
+              ..revision = 3
+              ..type = 'type'
+              ..updatedAt = 'updatedAt'
+              ..updatedBy = 'updatedBy'
+              ..version = 1,
+          ).toBuilder()
+          ..items = BuiltList<ContentType>.from(
+            [contentType],
+          ).toBuilder(),
+      );
+
+      expect(value, otherValue);
+    });
+
+    test('reports if not same', () {
+      final field = Field(
+        (b) => b
+          ..disabled = true
+          ..id = 'id'
+          ..isRequired = true
+          ..itemType = FieldType.boolean
+          ..localized = true
+          ..name = 'name'
+          ..type = FieldType.asset,
+      );
+
+      final contentType = ContentType(
+        (b) => b
+          ..description = 'description'
+          ..name = 'name'
+          ..sys = SystemFields(
+            (b) => b
+              ..contentType = 'contentType'
+              ..contentTypeId = 'contentTypeId'
+              ..createdAt = 'createdAt'
+              ..createdBy = 'createdBy'
+              ..firstPublishedAt = 'firstPublishedAt'
+              ..id = 'id'
+              ..locale = 'locale'
+              ..publishedAt = 'publishedAt'
+              ..publishedBy = 'publishedBy'
+              ..publishedCounter = 1
+              ..publishedVersion = 2
+              ..revision = 3
+              ..type = 'type'
+              ..updatedAt = 'updatedAt'
+              ..updatedBy = 'updatedBy'
+              ..version = 1,
+          ).toBuilder()
+          ..fields = BuiltList<Field>.from(
+            [field],
+          ).toBuilder(),
+      );
+
+      final value = ContentTypeResponse(
+        (b) => b
+          ..limit = 0
+          ..skip = 0
+          ..total = 100
+          ..sys = SystemFields(
+            (b) => b
+              ..contentType = 'contentType'
+              ..contentTypeId = 'contentTypeId'
+              ..createdAt = 'createdAt'
+              ..createdBy = 'createdBy'
+              ..firstPublishedAt = 'firstPublishedAt'
+              ..id = 'id'
+              ..locale = 'locale'
+              ..publishedAt = 'publishedAt'
+              ..publishedBy = 'publishedBy'
+              ..publishedCounter = 1
+              ..publishedVersion = 2
+              ..revision = 3
+              ..type = 'type'
+              ..updatedAt = 'updatedAt'
+              ..updatedBy = 'updatedBy'
+              ..version = 1,
+          ).toBuilder()
+          ..items = BuiltList<ContentType>.from(
+            [contentType],
+          ).toBuilder(),
+      );
+
+      final otherValue = value.rebuild(
+        (b) => b..sys.version = -1,
+      );
+
+      expectMismatch(value, otherValue, '');
     });
   });
 }
--- package/test/models/content_type_test.dart
+++ package/test/models/content_type_test.dart
@@ -1,3 +1,5 @@
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value_test/matcher.dart';
 import 'package:contentful_dart/contentful_dart.dart';
 import 'package:test/test.dart';
 
@@ -29,6 +31,167 @@ void main() {
       final contentType = ContentType.fromJson(loadFixture('content_type'));
 
       expect(contentType.contentId, equals('person'));
+    });
+
+    test('built_value matcher', () {
+      final field = Field(
+        (b) => b
+          ..disabled = true
+          ..id = 'id'
+          ..isRequired = true
+          ..itemType = FieldType.boolean
+          ..localized = true
+          ..name = 'name'
+          ..type = FieldType.asset,
+      );
+
+      final value = ContentType(
+        (b) => b
+          ..description = 'description'
+          ..name = 'name'
+          ..sys = SystemFields(
+            (b) => b
+              ..contentType = 'contentType'
+              ..contentTypeId = 'contentTypeId'
+              ..createdAt = 'createdAt'
+              ..createdBy = 'createdBy'
+              ..firstPublishedAt = 'firstPublishedAt'
+              ..id = 'id'
+              ..locale = 'locale'
+              ..publishedAt = 'publishedAt'
+              ..publishedBy = 'publishedBy'
+              ..publishedCounter = 1
+              ..publishedVersion = 2
+              ..revision = 3
+              ..type = 'type'
+              ..updatedAt = 'updatedAt'
+              ..updatedBy = 'updatedBy'
+              ..version = 1,
+          ).toBuilder()
+          ..fields = BuiltList<Field>.from(
+            [field],
+          ).toBuilder(),
+      );
+
+      expect(value, equalsBuilt(value));
+    });
+
+    test('compared value matcher', () {
+      final field = Field(
+        (b) => b
+          ..disabled = true
+          ..id = 'id'
+          ..isRequired = true
+          ..itemType = FieldType.boolean
+          ..localized = true
+          ..name = 'name'
+          ..type = FieldType.asset,
+      );
+
+      final value = ContentType(
+        (b) => b
+          ..description = 'description'
+          ..name = 'name'
+          ..sys = SystemFields(
+            (b) => b
+              ..contentType = 'contentType'
+              ..contentTypeId = 'contentTypeId'
+              ..createdAt = 'createdAt'
+              ..createdBy = 'createdBy'
+              ..firstPublishedAt = 'firstPublishedAt'
+              ..id = 'id'
+              ..locale = 'locale'
+              ..publishedAt = 'publishedAt'
+              ..publishedBy = 'publishedBy'
+              ..publishedCounter = 1
+              ..publishedVersion = 2
+              ..revision = 3
+              ..type = 'type'
+              ..updatedAt = 'updatedAt'
+              ..updatedBy = 'updatedBy'
+              ..version = 1,
+          ).toBuilder()
+          ..fields = BuiltList<Field>.from(
+            [field],
+          ).toBuilder(),
+      );
+
+      final otherValue = ContentType(
+        (b) => b
+          ..description = 'description'
+          ..name = 'name'
+          ..sys = SystemFields(
+            (b) => b
+              ..contentType = 'contentType'
+              ..contentTypeId = 'contentTypeId'
+              ..createdAt = 'createdAt'
+              ..createdBy = 'createdBy'
+              ..firstPublishedAt = 'firstPublishedAt'
+              ..id = 'id'
+              ..locale = 'locale'
+              ..publishedAt = 'publishedAt'
+              ..publishedBy = 'publishedBy'
+              ..publishedCounter = 1
+              ..publishedVersion = 2
+              ..revision = 3
+              ..type = 'type'
+              ..updatedAt = 'updatedAt'
+              ..updatedBy = 'updatedBy'
+              ..version = 1,
+          ).toBuilder()
+          ..fields = BuiltList<Field>.from(
+            [field],
+          ).toBuilder(),
+      );
+
+      expect(value, otherValue);
+    });
+
+    test('reports if not same', () {
+      final field = Field(
+        (b) => b
+          ..disabled = true
+          ..id = 'id'
+          ..isRequired = true
+          ..itemType = FieldType.boolean
+          ..localized = true
+          ..name = 'name'
+          ..type = FieldType.asset,
+      );
+
+      final value = ContentType(
+        (b) => b
+          ..description = 'description'
+          ..name = 'name'
+          ..sys = SystemFields(
+            (b) => b
+              ..contentType = 'contentType'
+              ..contentTypeId = 'contentTypeId'
+              ..createdAt = 'createdAt'
+              ..createdBy = 'createdBy'
+              ..firstPublishedAt = 'firstPublishedAt'
+              ..id = 'id'
+              ..locale = 'locale'
+              ..publishedAt = 'publishedAt'
+              ..publishedBy = 'publishedBy'
+              ..publishedCounter = 1
+              ..publishedVersion = 2
+              ..revision = 3
+              ..type = 'type'
+              ..updatedAt = 'updatedAt'
+              ..updatedBy = 'updatedBy'
+              ..version = 1,
+          ).toBuilder()
+          ..fields = BuiltList<Field>.from(
+            [field],
+          ).toBuilder(),
+      );
+
+      final otherValue = value.rebuild(
+        (b) => b..sys.version = -1,
+      );
+
+      expectMismatch(value, otherValue, '');
     });
   });
 }
