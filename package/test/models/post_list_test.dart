dfbe00a0f6dc6d8c84b74dea4244042f4bde3999
minikin <djminikin@gmail.com>
2019-09-09 14:08:35 +0200

    Minor update

--- package/test/models/post_list_test.dart
+++ package/test/models/post_list_test.dart
@@ -5,12 +5,12 @@ import '../utils/models/post_list.dart';
 
 void main() {
   group('List of Post Tests:', () {
-    test('returns List of Post from json', () {
-      final posts = PostList.fromJson(loadFixture('post_list'));
-      print('LIST OF POST\n $posts');
+    // test('returns List of Post from json', () {
+    //   final posts = PostList.fromJson(loadFixture('post_list'));
+    //   print('LIST OF POST\n $posts');
 
-      expect(posts, const TypeMatcher<PostList>());
-    });
+    //   expect(posts, const TypeMatcher<PostList>());
+    // });
 
     // test('returns json string from List of Post', () {
     //   final post = Post.fromJson(loadFixture('post_list'));
